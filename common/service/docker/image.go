d401f66f548be7e09edb4d5f056051390eb58557
rxwyun <1120309488@qq.com>
2026-02-28 16:29:48 +0800

    优化从 oci load 镜像

--- common/service/docker/image.go
+++ common/service/docker/image.go
@@ -13,6 +13,11 @@ import (
 	"sort"
 	"strings"
 
+	imagecopy "github.com/containers/image/v5/copy"
+	"github.com/containers/image/v5/docker/daemon"
+	"github.com/containers/image/v5/oci/archive"
+	"github.com/containers/image/v5/signature"
+	"github.com/containers/image/v5/types"
 	"github.com/donknap/dpanel/common/function"
 	"github.com/donknap/dpanel/common/types/fs"
 	"github.com/mcuadros/go-version"
@@ -130,59 +135,35 @@ func getFileListFromTar(tarReader *tar.Reader) (files []*fs.FileData, err error)
 	return files, nil
 }
 
-//func (self Client) ImageLoadFromOci(ctx context.Context, ociPath string, imageName string) error {
-//	tarPath := filepath.Join(filepath.Dir(ociPath), function.Md5(ociPath)+".tar")
-//	defer os.Remove(tarPath)
-//	err := ExtractCurPlatformImageTarFromOci(ctx, ociPath, tarPath, imageName)
-//	if err != nil {
-//		return err
-//	}
-//	importFile, err := imports.NewFileImport("", imports.WithImportTarFile(tarPath))
-//	if err != nil {
-//		return err
-//	}
-//	defer importFile.Close()
-//
-//	_, err = self.Client.ImageLoad(ctx,
-//		importFile.Reader(),
-//	)
-//	return err
-//}
-//
-//func ExtractCurPlatformImageTarFromOci(ctx context.Context, ociPath string, tarPath string, imageName string) error {
-//	srcRef, err := archive.NewReference(ociPath, "")
-//	if err != nil {
-//		return err
-//	}
-//	if imageName != "" {
-//		tarPath = tarPath + ":" + imageName
-//	}
-//	destRef, err := dockerarchive.ParseReference(tarPath)
-//	if err != nil {
-//		return err
-//	}
-//
-//	defaultPolicy := &signature.Policy{
-//		Default: []signature.PolicyRequirement{
-//			signature.NewPRInsecureAcceptAnything(),
-//		},
-//	}
-//	policyContext, err := signature.NewPolicyContext(defaultPolicy)
-//	if err != nil {
-//		return err
-//	}
-//	defer policyContext.Destroy()
-//
-//	currentOS, currentArch := function.CurrentSystemPlatform()
-//	sysCtx := &types.SystemContext{
-//		ArchitectureChoice: currentArch,
-//		OSChoice:           currentOS,
-//	}
-//	copyOptions := &copy.Options{
-//		SourceCtx:      sysCtx,
-//		DestinationCtx: sysCtx,
-//	}
-//	_, err = copy.Image(ctx, policyContext, destRef, srcRef, copyOptions)
-//
-//	return err
-//}
+func (self Client) ImageLoadFromOci(ctx context.Context, ociPath string, imageName string) error {
+	srcRef, err := archive.NewReference(ociPath, "")
+	if err != nil {
+		return err
+	}
+	destRef, err := daemon.ParseReference(imageName)
+	if err != nil {
+		return err
+	}
+
+	policyContext, err := signature.NewPolicyContext(&signature.Policy{
+		Default: []signature.PolicyRequirement{signature.NewPRInsecureAcceptAnything()},
+	})
+	if err != nil {
+		return err
+	}
+	defer policyContext.Destroy()
+
+	currentOS, currentArch := function.CurrentSystemPlatform()
+	sysCtx := &types.SystemContext{
+		ArchitectureChoice: currentArch,
+		OSChoice:           currentOS,
+		DockerDaemonHost:   self.Client.DaemonHost(),
+	}
+
+	_, err = imagecopy.Image(ctx, policyContext, destRef, srcRef, &imagecopy.Options{
+		SourceCtx:      sysCtx,
+		DestinationCtx: sysCtx,
+	})
+
+	return err
+}
